6b948bf82f715ff761fd66be30ba936763112467
Anupam Pandey <theanupamofficial@gmail.com>
2023-02-24 17:02:10 +0545

    :hammer: Project developed

---
+++ src/main/java/com/example/taskdoc/TaskdocApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskdocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskdoc/TaskdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
